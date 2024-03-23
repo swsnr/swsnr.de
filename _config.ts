7959c1a84f50831ab3544c70af9a8ee97e13e1ed
Sebastian Wiesner <sebastian@swsnr.de>
2024-03-23 10:48:45 +0100

    Add relative links and heading anchors

--- _config.ts
+++ _config.ts
@@ -8,17 +8,37 @@ import lume from "lume/mod.ts";
 import liquid from "lume/plugins/liquid.ts";
 import date from "lume/plugins/date.ts";
 import relative_urls from "lume/plugins/relative_urls.ts";
+import resolve_urls from "lume/plugins/resolve_urls.ts";
+
+import anchor from "npm:markdown-it-anchor";
 
 import title_from_heading from "./plugins/title-from-content.ts";
 
 const site = lume({
   src: "src",
   location: new URL("https://swsnr.de"),
+}, {
+  markdown: {
+    // Remove default plugins (definition lists, attributes) to improve compatibility with commonmark
+    useDefaultPlugins: false,
+    // Add anchors to all headings.
+    plugins: [[anchor, { level: 2 }]],
+    options: {
+      breaks: false,
+      html: true,
+    },
+  },
 });
 
+// Enable liquid templates.
 site.use(liquid());
+// Make all internal URLs relative.
 site.use(relative_urls());
+// Resolve all URLs to source files to the corresponding target file
+site.use(resolve_urls());
+// Add filter for date formatting with global date settings
 site.use(date());
+// Extract page title from first heading
 site.use(title_from_heading());
 
 export default site;
