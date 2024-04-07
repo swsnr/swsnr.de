bb3f22ad0dbeee56eea77155f5fcdb864cb7318c
Sebastian Wiesner <sebastian@swsnr.de>
2024-04-07 22:02:16 +0200

    Setup redirects for lunaryorn.com

--- serve.ts
+++ serve.ts
@@ -7,6 +7,7 @@
 import Server from "lume/core/server.ts";
 import www from "lume/middlewares/www.ts";
 import notfound from "lume/middlewares/not_found.ts";
+import redirects from "lume/middlewares/redirects.ts";
 
 const server = new Server({
   port: 8000,
@@ -16,6 +17,37 @@ const server = new Server({
 server.use(www({
   add: false, // false to remove, true to add it.
 }));
+// Redirect old URLs on lunaryorn.com
+const redirectOldUrls = redirects({
+  redirects: {
+    "/2014/04/15/calling-python-from-haskell": "/calling-python-from-haskell/",
+    "/2014/07/02/autoloads-in-emacs-lisp": "/autoloads-in-emacs-lisp/",
+    "/2014/08/12/emacs-script-pitfalls/": "/emacs-script-pitfalls/",
+  },
+});
+server.use((request, next, info) => {
+  const url = new URL(request.url);
+  if (url.hostname === "lunaryorn.com") {
+    return redirectOldUrls(request, next, info);
+  } else {
+    return next(request);
+  }
+});
+server.use(async (request, next) => {
+  const url = new URL(request.url);
+  if (url.hostname === "lunaryorn.com") {
+    // Redirect to new hostname
+    url.hostname = "swsnr.de";
+    return new Response(null, {
+      status: 301,
+      headers: {
+        location: url.href,
+      },
+    });
+  } else {
+    return await next(request);
+  }
+});
 server.use(notfound());
 
 server.start();
