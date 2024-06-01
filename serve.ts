edc358e748875f66b673326f9cfd6a67d3ddead5
Sebastian Wiesner <sebastian@swsnr.de>
2024-06-01 18:18:33 +0200

    Add redirect for emacs post

--- serve.ts
+++ serve.ts
@@ -23,6 +23,7 @@ const redirectOldUrls = redirects({
     "/2014/04/15/calling-python-from-haskell": "/calling-python-from-haskell/",
     "/2014/07/02/autoloads-in-emacs-lisp": "/autoloads-in-emacs-lisp/",
     "/2014/08/12/emacs-script-pitfalls/": "/emacs-script-pitfalls/",
+    "/posts/emacs-script-pitfalls.html": "/emacs-script-pitfalls/",
   },
 });
 server.use((request, next, info) => {
